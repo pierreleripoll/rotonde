2ba252df665814ec72d4a47f1cd170cd646974ee
pierreleripoll <pierreripoll13@gmail.com>
2018-05-02 11:14:20 +0200

    redirect to admin_log when no cookies

--- newTest.py
+++ newTest.py
@@ -66,10 +66,14 @@ def admin_log():
 
 @app.route('/admin', methods=['GET','POST'])
 def admin():
-    if request.method=='GET':
-        return render_template('admin.html')
-    if request.method=='POST':
-        return redirect('/')
+    pseudo = request.cookies.get("pseudo")
+    if pseudo :
+        if request.method=='GET':
+            return render_template('admin.html')
+        if request.method=='POST':
+            return redirect('/')
+    else :
+        return redirect('/admin_log')
 
 
 @app.route('/calendrier', methods=['GET','POST'])
