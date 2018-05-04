85a867e1bf3dcf160266baf64c39fcc2dc354c3a
Antoine Flamant <flamant.antoine@gmail.com>
2018-05-04 11:19:16 +0200

    Cleanup du code et ajout d'un systeme de panier

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-MERCI D'AVOIR LU
-PROJET PORTÉ PAR ARTHUR JN MATHILDE PIERRE ET GHILAINNN
-MERCI
+# Rotonde
+
+Site de réservation de places à la Rotonde
+
+# TODO
+
+- [ ] Implémenter `get_place_by_id`
+- [ ] Choisir un frontend
+- [ ] gérer la reservation dans la base de données
+
+Vous pouvez ajouter les TODOs ici ! ;)
+
+# Questions sur le projet
+
+## Antoine
+
+- Les champs de `calendrier` ne pourraient-ils pas être intégrés à la table `spectacle` ?
+- Ajouter `prix` dans la table `spectacle` ?
+- Avoir un champ `id` dans spectacle comme clé principale plutôt que `nom` ?
+- Avoir la table place liée à un nom et un spectacle, mais laisser date heure et tout dans spectacle ?
+- truc sur le nombre or something
+
+## Arthur
+
+## Jean-Nicolas
+
+## Mathilde
+
+## Pierre
+
+---
+
+Vous pouvez ajouter vos questions ici ! ;)
--- model.py
+++ model.py
@@ -1,11 +1,39 @@
 from sqlalchemy import *
 from sqlalchemy.sql import *
 
+# Connexion a la DB
 
-# Ici je définis des wrappers pour toutes les tables de la DB
+engine = create_engine('sqlite:///baseRotonde.db', echo=True)
+metadata = MetaData()
+
+
+spectacle = Table('spectacle', metadata,
+Column('nom', String, primary_key=True),
+Column('resume', String, nullable = True),
+Column('photo', String, nullable = True), #lien vers un file upload
+Column('liens', String, nullable = True))
+
+calendrier = Table('calendrier', metadata,
+Column('date', String, nullable = False),
+Column('nom', String, ForeignKey('spectacle.nom')),
+Column('placesRestantes', Integer, nullable = False))
+
+place = Table('places', metadata,
+Column('idPlace', Integer, autoincrement=True, primary_key=True),
+Column('date', String, ForeignKey('calendrier.date')),
+Column('nomUser', String, nullable = False))
+
+sessions = Table('sessions', metadata,
+Column('login',String,nullable=False),
+Column('password',String,nullable=False))
+
+metadata.create_all(engine)
+
+# Ici je definis des wrappers pour toutes les tables de la DB
 
 class Place:
-    def __init__(self,nom,date,heure,nombre):
+    def __init__(self,idPlace,nom,date,heure,nombre):
+        self.idPlace = idPlace
         self.nom = nom
         self.date = date
         self.heure = heure
@@ -15,23 +43,25 @@ class Place:
         self.nombre = nombre
 
 class Spectacle:
-
-    def __init__(self, nom, resume, prix, photos, liens):
+    def __init__(self, nom, resume, photos, liens):
         self.nom = nom
         self.resume = resume
-        self.prix = prix
+        # self.prix = prix
         self.photos = photos
         self.liens = liens
 
     def __repr__(self):
-        return "<Spectacle: %s, %s, %s€>"%(self.nom, self.resume, self.prix)
+        return "<Spectacle: %s, %s, %s>"%(self.nom, self.resume, self.prix)
 
+class Session:
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
 
-# Connexion a la DB
+    def __repr__(self):
+        return "<Session: %s, %s>"%(self.login, self.password)
 
-engine = create_engine('sqlite:///baseRotonde.db', echo=True)
-
-def connect(arg):
+def connect():
     conn = engine.connect()
     return conn
 
@@ -39,16 +69,30 @@ def connect(arg):
 # Renvoie une liste contenant tous les spectacles.
 def get_spectacles():
     conn = connect()
-    query = 'SELECT nom, resume, prix, photos, liens FROM spectacle'
+    query = 'SELECT nom, resume, photo, liens FROM spectacle'
     result = conn.execute(query)
 
     spectacles = []
 
     for row in result:
-        spectacle = Spectacle(row["nom"], row["resume"], row["prix"], row["photos"], row["liens"])
-
+        spectacle = Spectacle(row["nom"], row["resume"], row["photo"], row["liens"])
         spectacles.append(spectacle)
 
     conn.close()
 
     return spectacles
+
+def get_sessions():
+    conn = connect()
+    query = 'select login, password from sessions'
+    result = conn.execute(query)
+
+    sessions = []
+
+    for row in result:
+        sess = Session(row["login"], row["password"])
+        sessions.append(sess)
+
+    conn.close()
+
+    return sessions
