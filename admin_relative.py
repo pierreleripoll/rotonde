392a227f07f456e47e1101e852fe31df7c9c55ea
Tas-Hur <arthur.navarro@laposte.net>
2018-05-09 19:29:01 +0200

    adding the framents

---
+++ admin_relative.py
@@ -0,0 +1,59 @@
+from flask import *
+from flask import current_app as app
+from sqlalchemy import *
+from sqlalchemy.sql import *
+from werkzeug.utils import secure_filename
+import os
+import re
+from model import*
+from jinja2 import TemplateNotFound
+
+admin_relative = Blueprint('admin_relative', __name__,
+                        template_folder='templates',static_folder = 'static')
+
+## PAGE ADMIN
+@admin_relative.route('/admin', methods=['GET','POST'])
+def admin():
+
+    if 'admin' in session :
+        if request.method=='GET':
+            return render_template('admin.html',pseudo = session["pseudo"])
+        if request.method=='POST':
+            if request.form["bouton"]=="logout":
+                session.pop("pseudo",None)
+                session.pop("admin",None)
+                session.clear()
+                return redirect(url_for('logout'))
+            if request.form["bouton"]=="create":
+                return redirect(url_for('gestion_spectacle.set_spectacle',nomSpectacle='nouveauSpectacle'))
+
+    else :
+        return redirect(url_for('admin_relative.admin_log'))
+
+## LOGIN ADMIN
+@admin_relative.route('/admin_log', methods=['GET','POST'])
+def admin_log():
+
+    if request.method=='GET':
+        return render_template('admin_log.html')
+
+    if request.method=='POST':
+
+        sessions = get_sessions()
+
+        login = request.form["login"]
+        password = request.form["password"]
+
+        for sess in sessions :
+            if login == sess.login and password == sess.password:
+                welcomeString = "WELCOME "+login.upper()
+                print("\n\n")
+                print(len(welcomeString)*'*')
+                print(welcomeString)
+                print(len(welcomeString)*'*')
+                print("\n\n")
+                session['pseudo']=login.upper()
+                session['admin']="true"
+                print(session)
+
+        return redirect(url_for('admin_relative.admin'))
---
+++ panier_relative.py
@@ -0,0 +1,45 @@
+from flask import *
+from flask import current_app as app
+from sqlalchemy import *
+from sqlalchemy.sql import *
+from werkzeug.utils import secure_filename
+import os
+import re
+from model import*
+from jinja2 import TemplateNotFound
+
+panier_relative = Blueprint('panier_relative', __name__,
+                        template_folder='templates',static_folder = 'static')
+
+## PANIER
+@panier_relative.route('/panier', methods=['GET'])
+def panier():
+    """TODO: Display the contents of the shopping cart."""
+    if "cart" not in session:
+        # flash("There is nothing in your cart.")
+        return render_template("cart.html", display_cart = {}, total = 0)
+    else:
+        items = session["cart"]
+        dict_of_places = {}
+
+        total_price = 0
+
+        for item in items:
+            # place = get_place_by_id() #A modifier
+            place = Place(item,'test','osef','osef',1)
+            total_price += 1 # A modifier
+            if place.idPlace in dict_of_places:
+                dict_of_places[place.idPlace]["qty"] += 1
+            else:
+                dict_of_places[place.idPlace] = {"qty":1, "name": "spectacle" + str(place.idPlace), "price": 1}
+
+        return render_template("cart.html", display_cart = dict_of_places, total = total_price)
+
+@panier_relative.route('/add_to_cart/<int:id>', methods=['POST','GET']) #Provisoire, enlever le get
+def add_to_cart(id):
+    if "cart" not in session:
+        session["cart"] = []
+    session["cart"].append(id)
+    flash("Successfully added to cart!")
+    return redirect("/panier")
+
