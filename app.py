79ef435eab74a7f5825f8717834373719d550bcd
Antoine Flamant <flamant.antoine@gmail.com>
2018-05-24 12:01:34 +0200

    Whaaaat ça fonctionne ! (un peu)

--- app.py
+++ app.py
@@ -39,6 +39,10 @@ db.init_app(app)
 # 	db.session.commit()
 
 
+@app.route('/templates/<name>')
+def month(name):
+	return render_template(name)
+
 #PAGE DE TEST JAVASCRIPT
 @app.route('/javascript')
 def javascript():
